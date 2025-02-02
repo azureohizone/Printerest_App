7fff535f25854080c03e285a13cdf7ba764dfdd1
Tongei <tann.tongei@gmail.com>
2025-02-02 15:38:53 +0700

    Change: Structure

--- lib/img/img.dart
+++ lib/img/img.dart
@@ -1,4 +1,4 @@
-List<String> vibe =[
+List<String> vibe = [
   "https://i.pinimg.com/736x/3b/dd/20/3bdd20d258f15f8fa33376ee3c21dec1.jpg",
   "https://i.pinimg.com/736x/9c/6f/d9/9c6fd9c779b0d382ce98150f8c560bae.jpg",
   "https://i.pinimg.com/736x/77/cd/d3/77cdd3a7f09ebe130ac60cb739504e4c.jpg",
@@ -10,10 +10,9 @@ List<String> vibe =[
   "https://i.pinimg.com/474x/a0/35/d4/a035d46f9e1a040b289f9a648ab682f1.jpg",
   "https://i.pinimg.com/474x/6a/cb/54/6acb542d7f9227e4d1039fc5c494bc36.jpg",
   "https://i.pinimg.com/474x/70/0d/d2/700dd2e51c4fd3425ab63e8d9f50e4a6.jpg",
-
 ];
 
-List<String> animals=[
+List<String> animals = [
   "https://i.pinimg.com/474x/d4/a6/a6/d4a6a6d1b827d4ab9d4d05e5f7a195d6.jpg",
   "https://i.pinimg.com/736x/9f/0d/78/9f0d785b5951f1f4e4b8cb3b5792f2b7.jpg",
   "https://i.pinimg.com/474x/7d/53/9f/7d539f8a92fdc5ed58c59327dde81315.jpg",
